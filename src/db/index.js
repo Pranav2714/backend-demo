6907e5061f1d0e9a1ceec1f38fcdeddbd4777f32
Pranav Mahamunkar <pranav.mahamunkar11@gmail.com>
2024-04-07 19:17:21 +0530

    adding cookie parser

---
+++ src/db/index.js
@@ -0,0 +1,33 @@
+// import mongoose from "mongoose";
+// import { DB_NAME } from "../constants.js";
+
+// const connectDB = async () => {
+//   try {
+//     const connectioninstance = await mongoose.connect(
+//       `${process.env.MONGODB_URI}/${DB_NAME}`
+//     );
+//     console.log(`MONGODB connected : ${connectioninstance.connection.host}`);
+//   } catch (error) {
+//     console.log("MONGODB connection error", error);
+//     process.exit(1);
+//   }
+// };
+// export default connectDB;
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MONGODB connection FAILED ", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
